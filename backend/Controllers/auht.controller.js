ab356b4c353dffec34ebf6e49a69a6541dabf047
TanveerAhmadDev <tanveeera84@gmail.com>
2025-10-17 21:53:37 -0700

    some Moreee Changes

--- backend/Controllers/auht.controller.js
+++ backend/Controllers/auht.controller.js
@@ -36,7 +36,7 @@ export const universalLogin = async (req, res) => {
       res.cookie("adminToken", accessToken, {
         httpOnly: true,
         secure: true, // MUST be true in production (HTTPS only)
-        sameSite: "None",
+        sameSite: "lax",
       });
 
       return res.status(200).json({
@@ -63,13 +63,13 @@ export const universalLogin = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "25m",
+      expiresIn: "50m",
     });
 
     res.cookie("userToken", token, {
       httpOnly: true,
       secure: true, // MUST be true in production (HTTPS only)
-      sameSite: "None",
+      sameSite: "lax",
     });
 
     let redirectUrl = "/";
@@ -174,13 +174,13 @@ export const logout = async (req, res) => {
   res.clearCookie("userToken", {
     httpOnly: true,
     secure: true, // MUST be true in production (HTTPS only)
-    sameSite: "None", // Must match the setting used when you set the cookie
+    sameSite: "lax", // Must match the setting used when you set the cookie
   });
 
   res.clearCookie("adminToken", {
     httpOnly: true,
     secure: true, // MUST be true in production (HTTPS only)
-    sameSite: "None",
+    sameSite: "lax",
   });
 
   res.json({ message: "Logged out successfully" });
